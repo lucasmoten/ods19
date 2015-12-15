84286703839221487ae6dfb266655e1383efd51d
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2015-12-15 14:47:42 -0500

    WIP

--- services/transfer/service.go
+++ services/transfer/service.go
@@ -2,6 +2,8 @@ package transfer
 
 import (
 	"errors"
+	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 
@@ -21,8 +23,32 @@ type TransferServiceImpl struct{}
 // Upload ...
 func (t TransferServiceImpl) Upload(r *multipart.Reader) error {
 
-	// TODO
+	log.Println("got this reader", r)
+	var partBytes int
+	for {
+		part, partErr := r.NextPart()
+		if partErr != nil {
+			if partErr == io.EOF {
+				break //just an eof...not an error
+			} else {
+				log.Printf("error getting a part %v", partErr)
+				// http.Error(w, "error getting a part", 500)
+				return partErr
+			}
+		} else {
+			if len(part.FileName()) > 0 {
+				//Could take an *indefinite* amount of time!!
+				buffer := make([]byte, 1024)
+				bytesRead, err := part.Read(buffer)
+				if err != nil {
+					return err
+				}
+				partBytes += bytesRead
+			}
 
+		}
+	}
+	log.Printf("Read this many bytes without error", partBytes)
 	return nil
 
 }
@@ -43,7 +69,7 @@ func MakeUploadEndpoint(svc TransferService) endpoint.Endpoint {
 
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
 
-		req := request.(http.Request)
+		req := request.(*http.Request)
 		reader, err := req.MultipartReader()
 		if err != nil {
 			return UploadResponse{err.Error()}, nil
