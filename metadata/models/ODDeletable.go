0e6cc4b910af318c0d9af5fac4510be9814ba625
Lucas Moten <lucas.moten@deciphertechstudios.com>
2016-01-07 15:50:50 -0500

    Refactored database logic from test code into callable functions as mysql.dao to simplify main. Filtering sensitive fields during json marshalling. Added Resultset struct.

--- metadata/models/ODDeletable.go
+++ metadata/models/ODDeletable.go
@@ -5,7 +5,7 @@ ODDeletable is a nestable structure defining the attributes tracked for
 Object Drive elements that may be deletedBy
 */
 type ODDeletable struct {
-	IsDeleted   bool       `db:"isDeleted"`
-	DeletedDate NullTime   `db:"deletedDate"`
-	DeletedBy   NullString `db:"deletedBy"`
+	IsDeleted   bool       `db:"isDeleted" json:"-"`
+	DeletedDate NullTime   `db:"deletedDate" json:"-"`
+	DeletedBy   NullString `db:"deletedBy" json:"-"`
 }
