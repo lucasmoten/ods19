a52ab83a6049be239d3b1d7fe56b31b030089904
Lucas Moten <lucas.moten@deciphertechstudios.com>
2016-01-24 13:45:42 -0500

    WIP GetChildObjectsWithPropertiesByOwner now compilable. 2 tests failing

--- cmd/metadataconnector/libs/dao/mysql.GetChildObjectsWithPropertiesByOwner.go
+++ cmd/metadataconnector/libs/dao/mysql.GetChildObjectsWithPropertiesByOwner.go
@@ -11,13 +11,11 @@ import (
 func GetChildObjectsWithPropertiesByOwner(db *sqlx.DB, orderByClause string, pageNumber int, pageSize int, object *models.ODObject, owner string) (models.ODObjectResultset, error) {
 	response, err := GetChildObjectsByOwner(db, orderByClause, pageNumber, pageSize, object, owner)
 	if err != nil {
-		print(err.Error())
 		return response, err
 	}
 	for _, responseObject := range response.Objects {
 		properties, err := GetPropertiesForObject(db, responseObject.ID)
 		if err != nil {
-			print(err.Error())
 			return response, err
 		}
 		responseObject.Properties = *properties
