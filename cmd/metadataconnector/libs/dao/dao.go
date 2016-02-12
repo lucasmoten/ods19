4fa7ce41789669e60a5835746d458cd85871dbce
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-02-12 11:35:56 -0500

    Move fake DAO to its own file

--- cmd/metadataconnector/libs/dao/dao.go
+++ cmd/metadataconnector/libs/dao/dao.go
@@ -35,32 +35,6 @@ type DAO interface {
 	// UpdatePermission(permission *models.ODObjectPermission) error
 }
 
-// FakeDAO is suitable for tests. Add fields to this struct to hold fake
-// reponses for each of the methods that FakeDAO will implement. These fake
-// response fields can be explicitly set in tests.
-type FakeDAO struct {
-	Object            *models.ODObject
-	ObjectPermissions []models.ODObjectPermission
-	ObjectProperites  []models.ODObjectPropertyEx
-	// TODO: all required responses should be fields.
-}
-
-// GetObject for FakeDAO.
-func (fake *FakeDAO) GetObject(object *models.ODObject, loadProperties bool) (*models.ODObject, error) {
-	// return what we set on the field
-	return fake.Object, nil
-}
-
-// GetPermissionsForObject for FakeDAO.
-func (fake *FakeDAO) GetPermissionsForObject(object *models.ODObject) ([]models.ODObjectPermission, error) {
-	return fake.ObjectPermissions, nil
-}
-
-// GetPropertiesForObject for FakeDAO.
-func (fake *FakeDAO) GetPropertiesForObject(object *models.ODObject) ([]models.ODObjectPropertyEx, error) {
-	return fake.ObjectProperites, nil
-}
-
 // DataAccessLayer is a concrete DAO implementation with a true db conn.
 type DataAccessLayer struct {
 	MetadataDB *sqlx.DB
---
+++ cmd/metadataconnector/libs/dao/fake.go
@@ -0,0 +1,29 @@
+package dao
+
+import "decipher.com/oduploader/metadata/models"
+
+// FakeDAO is suitable for tests. Add fields to this struct to hold fake
+// reponses for each of the methods that FakeDAO will implement. These fake
+// response fields can be explicitly set in tests.
+type FakeDAO struct {
+	Object            *models.ODObject
+	ObjectPermissions []models.ODObjectPermission
+	ObjectProperites  []models.ODObjectPropertyEx
+	// TODO: all required responses should be fields.
+}
+
+// GetObject for FakeDAO.
+func (fake *FakeDAO) GetObject(object *models.ODObject, loadProperties bool) (*models.ODObject, error) {
+	// return what we set on the field
+	return fake.Object, nil
+}
+
+// GetPermissionsForObject for FakeDAO.
+func (fake *FakeDAO) GetPermissionsForObject(object *models.ODObject) ([]models.ODObjectPermission, error) {
+	return fake.ObjectPermissions, nil
+}
+
+// GetPropertiesForObject for FakeDAO.
+func (fake *FakeDAO) GetPropertiesForObject(object *models.ODObject) ([]models.ODObjectPropertyEx, error) {
+	return fake.ObjectProperites, nil
+}
