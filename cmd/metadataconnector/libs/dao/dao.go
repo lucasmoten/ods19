573490b7b721ecf7887096832dec5e905cf64fb7
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-02-12 11:32:05 -0500

    Stubbed out complete DAO interface

--- cmd/metadataconnector/libs/dao/dao.go
+++ cmd/metadataconnector/libs/dao/dao.go
@@ -19,9 +19,20 @@ type DAO interface {
 	// GetChildObjectsWithProperties(orderByClause string, pageNumber int, pageSize int, object *models.ODObject) (models.ODObjectResultset, error)
 	// GetChildObjectsWithPropertiesByOwner(orderByClause string, pageNumber int, pageSize int, object *models.ODObject, owner string) (models.ODObjectResultset, error)
 	GetObject(object *models.ODObject, loadProperties bool) (*models.ODObject, error)
+	// GetObjectProperty(objectProperty *models.ODObjectPropertyEx) (*models.ODObjectPropertyEx, error)
+	// GetObjectType(objectType *models.ODObjectType) (*models.ODObjectType, error)
+	// GetObjectTypeByName(typeName string, addIfMissing bool, createdBy string) (models.ODObjectType, error)
 	GetPermissionsForObject(object *models.ODObject) ([]models.ODObjectPermission, error)
 	GetPropertiesForObject(object *models.ODObject) ([]models.ODObjectPropertyEx, error)
-	// TODO: All required methods.
+	// GetRootObjects(orderByClause string, pageNumber int, pageSize int) (models.ODObjectResultset, error)
+	// GetRootObjectsByOwner(orderByClause string, pageNumber int, pageSize int, owner string) (models.ODObjectResultset, error)
+	// GetRootObjectsWithProperties(orderByClause string, pageNumber int, pageSize int) (models.ODObjectResultset, error)
+	// GetRootObjectsWithPropertiesByOwner(orderByClause string, pageNumber int, pageSize int, owner string) (models.ODObjectResultset, error)
+	// GetUserByDistinguishedName(user *models.ODUser) (*models.ODUser, error)
+	// GetUsers() ([]string, error)
+	// UpdateObject(object *models.ODObject, acm *models.ODACM) error
+	// UpdateObjectProperty(objectProperty *models.ODObjectPropertyEx) error
+	// UpdatePermission(permission *models.ODObjectPermission) error
 }
 
 // FakeDAO is suitable for tests. Add fields to this struct to hold fake
--- cmd/metadataconnector/libs/dao/mysql.GetUsers.go
+++ cmd/metadataconnector/libs/dao/mysql.GetUsers.go
@@ -1,15 +1,14 @@
 package dao
 
 import (
-	//"database/sql"
-	//"fmt"
-	//"decipher.com/oduploader/metadata/models"
-	"github.com/jmoiron/sqlx"
 	"log"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // GetUsers retrieves the list of users.  There is no filtering by ACM yet.
 func GetUsers(db *sqlx.DB) ([]string, error) {
+	// TODO this should return a User struct
 	//XXX this is no good when the list is very large!!!!
 	var result []string
 	getUsersStatement := `select distinguishedName from user`
