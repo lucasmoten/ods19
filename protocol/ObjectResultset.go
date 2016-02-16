2408fd8580b97d1532b9bbb8b3c275fce1a2729b
Lucas Moten <lucas.moten@deciphertechstudios.com>
2016-02-16 14:12:32 -0500

    WIP for setting up structs used by API

---
+++ protocol/ObjectResultset.go
@@ -0,0 +1,11 @@
+package protocol
+
+// ObjectResultset encapsulates the Object defined herein as an array with
+// resultset metric information to expose page size, page number, total rows,
+// and page count information when retrieving from the data store
+type ObjectResultset struct {
+	// Resultset contains meta information about the resultset
+	Resultset
+	// Objects contains the list of objects in this (page of) results.
+	Objects []Object
+}
