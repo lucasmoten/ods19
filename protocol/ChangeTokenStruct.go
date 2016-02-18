37f4ed632277d80d16cca038ca9e4635f2ca78ed
Lucas Moten <lucas.moten@deciphertechstudios.com>
2016-02-18 11:29:02 -0500

    WIP moveObject test shell

---
+++ protocol/ChangeTokenStruct.go
@@ -0,0 +1,10 @@
+package protocol
+
+// ChangeTokenStruct is a nestable structure defining the ChangeToken attribute
+// for items in Object Drive
+type ChangeTokenStruct struct {
+	// ChangeToken is generated value which is assigned at the database. API calls
+	// performing updates must provide the changeToken to be verified against the
+	// existing value on record to prevent accidental overwrites.
+	ChangeToken string `db:"changeToken" json:"changeToken"`
+}
