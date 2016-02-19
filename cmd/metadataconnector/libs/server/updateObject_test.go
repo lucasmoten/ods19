2e23e389fc46fb7d23c6e12327538c7a52c0d7e5
Lucas Moten <lucas.moten@deciphertechstudios.com>
2016-02-19 15:14:25 -0500

    WIP Added test for updateObject

---
+++ cmd/metadataconnector/libs/server/updateObject_test.go
@@ -0,0 +1,85 @@
+package server_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"testing"
+	"time"
+
+	"decipher.com/oduploader/protocol"
+)
+
+func TestUpdateObject(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	verboseOutput := testing.Verbose()
+	clientid := 0
+
+	if verboseOutput {
+		fmt.Printf("(Verbose Mode) Using client id %d", clientid)
+		fmt.Println()
+	}
+
+	// Create 1 folders under root
+	folder, err := makeFolderViaJSON("Test Folder for Update "+strconv.FormatInt(time.Now().Unix(), 10), clientid)
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	// Attempt to rename the folder
+	updateuri := host + "/service/metadataconnector/1.0/object/" + hex.EncodeToString(folder.ID) + "/properties"
+	folder.Name = "Test Folder Updated " + strconv.FormatInt(time.Now().Unix(), 10)
+	jsonBody, err := json.Marshal(folder)
+	if err != nil {
+		log.Printf("Unable to marshal json for request:%v", err)
+		t.Fail()
+		return
+	}
+	req, err := http.NewRequest("PUT", updateuri, bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Printf("Error setting up HTTP Request: %v", err)
+		t.Fail()
+		return
+	}
+	// do the request
+	transport := &http.Transport{TLSClientConfig: clients[clientid].Config}
+	client := &http.Client{Transport: transport}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("Unable to do request:%v", err)
+		t.Fail()
+		return
+	}
+	// process Response
+	if res.StatusCode != http.StatusOK {
+		log.Printf("bad status: %s", res.Status)
+		t.Fail()
+		return
+	}
+	decoder := json.NewDecoder(res.Body)
+	var updatedFolder protocol.Object
+	err = decoder.Decode(&updatedFolder)
+	if err != nil {
+		log.Printf("Error decoding json to Object: %v", err)
+		log.Println()
+		t.Fail()
+	}
+	if verboseOutput {
+		jsonData, err := json.MarshalIndent(updatedFolder, "", "  ")
+		if err != nil {
+			log.Printf("(Error in Verbose Mode) Error marshalling response back to json: %s", err.Error())
+			return
+		}
+		fmt.Println("Here is the response body:")
+		fmt.Println(string(jsonData))
+	}
+
+}
