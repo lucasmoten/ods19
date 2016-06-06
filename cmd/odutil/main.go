a3d97eb98167f1164c86b8e4e2c4956f8847c6ea
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-06-06 12:55:13 -0400

    Script for building an rpm.
    Expects ODRIVE_BINARY_DIR and ODRIVE_VERSION to be set. We can do this with
    Salt, or at the command line. ODRIVE_BINARY_DIR corresponds to
    **cmd/odrive**. This directory is expected to contain a
    centos-compatible binary as well as odrive.yml
    
    If you are on a centos machine with rpm-build installed, this script
    should produce an RPM that wraps a binary and the basic odrive.yml
    config. Binary will be unpacked into /usr/bin and config will be
    unpacked to /etc/odrive.
    
    Also updated odutil utility program to allow specifying a "key" to write
    to S3. Previously the key was the same as whatever file was passed to
    `input` flag. Now that we can write a custom key, we can have nested
    paths like:
    
    ```
    odrive-builds/rpms/odrive-1.0.x86_64.rpm
    ```
    
    Also add s3 library to govendor.

--- cmd/odutil/main.go
+++ cmd/odutil/main.go
@@ -11,6 +11,7 @@ var (
 	bucket = flag.String("bucket", "odrive-builds", "The bucket to target.")
 	input  = flag.String("input", "", "input file")
 	cmd    = flag.String("cmd", "upload", "Command to run.")
+	key    = flag.String("key", "", "Key up on S3 to write. defaults to the value passed to input")
 )
 
 const (
@@ -23,7 +24,7 @@ func main() {
 	flag.Parse()
 	switch *cmd {
 	case upload:
-		uploadRoutine(*input, *bucket)
+		uploadRoutine(*input, *bucket, *key)
 	case download:
 		downloadRoutine(*input, *bucket)
 	default:
@@ -32,11 +33,14 @@ func main() {
 
 }
 
-func uploadRoutine(input, bucket string) {
+func uploadRoutine(input, bucket, s3key string) {
 	// key will become the "filename" you see in the s3 bucket. Passing
 	// an entire filepath as the key will yield nested directory structures
 	// on s3 itself.
 	_, key := filepath.Split(input)
+	if s3key != "" {
+		key = s3key
+	}
 	fmt.Printf("Uploading file %s to bucket %s\n", input, bucket)
 	fmt.Printf("Extracted filename from input: %s\n", key)
 	MoveToS3(input, bucket, key)
