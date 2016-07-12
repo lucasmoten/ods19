39658799d77dadead0ef1c963c6707468dfa85a1
Rob Fielding <rob.fielding@deciphertechstudios.com>
2016-07-12 12:22:40 -0400

    rotate keys within a transaction

--- cmd/odrive-database/schema/procedure.rotate_keys.sql
+++ cmd/odrive-database/schema/procedure.rotate_keys.sql
@@ -2,10 +2,21 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS rotate_keys //
 CREATE PROCEDURE rotate_keys(IN oldMasterPass VARCHAR(255), IN newMasterPass VARCHAR(255), IN entropy VARCHAR(255))
 BEGIN 
+#Precondition: make sure that ALL rows have a MAC that currently checks out.  If run on unclean data,
+# it will make problems even harder to recover from.
+declare exit handler for sqlexception
+begin
+  rollback;
+end;
+declare exit handler for sqlwarning
+begin
+  rollback;
+end;
 #slightly dicey.... same IV, new key.  ok for now because underlying file keys being encrypted don't change,
 #while the masterkey does change.  so we follow the rule of not repeating same iv under a key againt different data.
 #If I could assign a randomm new iv but use the old one to plugin to one part and new one to plug into new part,
 #that would let me also change iv here.
+start transaction;
 update object_permission p set encryptKey = unhex( 
   bitwise256_xor( 
     new_keydecrypt( newMasterPass, hex(p.permissionIV)), 
@@ -16,5 +27,6 @@ update object_permission p set encryptKey = unhex(
   ) 
 );
 update object_permission p set permissionMAC = unhex( new_keymac(newMasterPass, p.grantee, p.allowCreate, p.allowRead, p.allowUpdate, p.allowDelete, p.allowShare, hex(encryptKey)));
+commit;
 END //
 DELIMITER ;
