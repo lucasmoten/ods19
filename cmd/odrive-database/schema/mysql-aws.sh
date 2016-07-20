5ece793035ed57c89390231208c7287c9616ee6c
Rob Fielding <rob.fielding@deciphertechstudios.com>
2016-07-20 12:24:18 -0400

    remove password for database from the code

--- cmd/odrive-database/schema/mysql-aws.sh
+++ cmd/odrive-database/schema/mysql-aws.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 set -e
 
-# This script will connect to mysql/mariadb on aws
+if [ "x$OD_DB_AWS_MYSQL_MASTER_USER" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_MASTER_USER=$OD_DB_USERNAME
+fi
 
-MYSQL_MASTER_USER=
-MYSQL_MASTER_PASSWORD=
-MYSQL_ENDPOINT=
-MYSQL_PORT="3306"
-MYSQL_DATABASE_NAME="metadatadb"
-MYSQL_SSL_CA_PATH="$GOPATH/src/decipher.com/object-drive-server/defaultcerts/aws/rds-combined-ca-bundle.pem"
+if [ "x$OD_DB_AWS_MYSQL_MASTER_PASSWORD" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_MASTER_PASSWORD=$OD_DB_PASSWORD
+fi
+
+if [ "x$OD_DB_AWS_MYSQL_ENDPOINT" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_ENDPOINT=$OD_DB_HOST
+fi
+
+if [ "x$OD_DB_AWS_MYSQL_PORT" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_PORT=$OD_DB_PORT
+fi
+
+if [ "x$OD_DB_AWS_MYSQL_DATABASE_NAME" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_DATABASE_NAME=$OD_DB_SCHEMA
+fi
+
+if [ "x$OD_DB_AWS_MYSQL_SSL_CA_PATH" == "x" ]
+then
+  export OD_DB_AWS_MYSQL_SSL_CA_PATH=$OD_DB_CA
+fi
 
 # Uncomment the following to use SSL
-SSL_CA="--ssl --ssl-ca $MYSQL_SSL_CA_PATH"
+SSL_CA="--ssl --ssl-ca $OD_DB_AWS_MYSQL_SSL_CA_PATH"
 
 # Uncomment the following to verify server cert
 SSL_VERIFY=--ssl-verify-server-cert
 
-mysql -h ${MYSQL_ENDPOINT} --protocol=tcp --database=${MYSQL_DATABASE_NAME} -u${MYSQL_MASTER_USER} -p${MYSQL_MASTER_PASSWORD} ${SSL_CA} ${SSL_VERIFY}
+mysql -h ${OD_DB_AWS_MYSQL_ENDPOINT} --protocol=tcp --database=${OD_DB_AWS_MYSQL_DATABASE_NAME} -u${OD_DB_AWS_MYSQL_MASTER_USER} -p${OD_DB_AWS_MYSQL_MASTER_PASSWORD} ${SSL_CA} ${SSL_VERIFY}
