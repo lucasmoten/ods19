4adaa9a90d8a8ddbe58574645c57357e93765687
Rob Fielding <rob.fielding@deciphertechstudios.com>
2016-07-21 15:20:01 -0400

    add in etag testing
    respond to comments

--- cmd/odrive/libs/server/getObjectStream_test.go
+++ cmd/odrive/libs/server/getObjectStream_test.go
@@ -12,6 +12,68 @@ import (
 	"decipher.com/object-drive-server/util/testhelpers"
 )
 
+func TestEtag(t *testing.T) {
+	clientID := 5
+	b := []byte(`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@`)
+
+	file, cleanup := testhelpers.GenerateTempFileFromBytes(b, t)
+	defer cleanup()
+
+	req, err := testhelpers.NewCreateObjectPOSTRequest(host, "", file)
+	if err != nil {
+		t.Errorf("Failure from NewCreateObjectPOSTRequest: %v\n", err)
+	}
+
+	client := httpclients[clientID]
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Unable to do request:%v\n", err)
+	}
+
+	var responseObject protocol.Object
+	err = util.FullDecode(res.Body, &responseObject)
+	if err != nil {
+		t.Errorf("Could not decode reponse from createObject: %v\n", err)
+	}
+
+	//Ask for it in order to get the eTag and a 200
+	req2, err := testhelpers.NewGetObjectStreamRequest(responseObject.ID, "", host)
+	if err != nil {
+		t.Errorf("Failure from redo get object stream: %v\n", err)
+	}
+	client2 := httpclients[clientID]
+	res2, err := client2.Do(req2)
+	if err != nil {
+		t.Errorf("Unable to do re request:%v\n", err)
+	}
+	eTag := res2.Header.Get("Etag")
+	t.Logf("we got eTag:%s", eTag)
+	if len(eTag) == 0 {
+		//We have no situation where a stream does not return an Etag now
+		t.Errorf("We did not get an Etag back")
+	}
+
+	if res2.StatusCode != http.StatusOK {
+		t.Errorf("bad status on get: %d", res2.StatusCode)
+	}
+
+	//Ask again with the eTag and get a 304
+	req3, err := testhelpers.NewGetObjectStreamRequest(responseObject.ID, "", host)
+	if err != nil {
+		t.Errorf("Failure from redo get object stream: %v\n", err)
+	}
+	req3.Header.Set("If-none-match", eTag)
+	client3 := httpclients[clientID]
+	res3, err := client3.Do(req3)
+	if err != nil {
+		t.Errorf("Unable to do re request:%v\n", err)
+	}
+
+	if res3.StatusCode != http.StatusNotModified {
+		t.Errorf("the data was not modified, and we sent an eTag, yet did not get a 304. %d instead", res3.StatusCode)
+	}
+}
+
 func TestUploadAndGetByteRange(t *testing.T) {
 
 	b := []byte(`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@`)
