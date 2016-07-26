0dc0eda5a3a4b19e87594aa48b86ee14c81c4aa7
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-07-26 12:32:42 -0400

    Introduce OD_LOG_LOCATION env var for configurable logging location
    odrive will still log to stdout, but we need to provide a place to set a
    logging location that odrive's stdout stream will be redirected to. This
    happens in the Python init script packaged at /etc/init.d/odrive
    
    Default location will be /opt/odrive/log/object-drive.log if
    OD_LOGS_LOCATION not set.
    
    Refs #53
    
    Also remove unused cross-compilation script

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+# Please review /etc/init.d/odrive for default logging location if OD_LOG_LOCATION is not set
+#
+
 export OD_AAC_CA=
 export OD_AAC_CERT=
 export OD_AAC_HOST=
@@ -31,6 +35,8 @@ export OD_DB_USERNAME=
 export OD_DOCKERVM_OVERRIDE=
 export OD_DOCKERVM_PORT=
 export OD_ENCRYPT_MASTERKEY=
+export OD_LOG_LOCATION=
+export OD_SERVER_BASEPATH=
 export OD_SERVER_CA=
 export OD_SERVER_CERT=
 export OD_SERVER_KEY=
@@ -43,5 +49,4 @@ export OD_ZK_MYPORT=
 export OD_ZK_ROOT=
 export OD_ZK_TIMEOUT=
 export OD_ZK_URL=
-
 
