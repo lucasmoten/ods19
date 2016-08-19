5649d1512464b40b8dac79047e61927db8b59fc1
Rob Fielding <rob.fielding@deciphertechstudios.com>
2016-08-19 19:35:29 -0400

    run as root bug is too hard to test without making rpm to install locally

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf ~/rpmbuild
+cd ${ODRIVE_ROOT}/cmd/odrive-database
+tar cvfz odrive-schema-${ODRIVE_VERSION}.tar.gz schema
+cd ${ODRIVE_ROOT}
+( cd cmd/odrive && go build )
+( cd cmd/odrive && go build -o main )
+( cd cmd/odutil && go build )
+
+#build it
+${ODRIVE_ROOT}/scripts/prepare-rpm-env.sh
+cp ~/rpmbuild/RPMS/x86_64/odrive-${ODRIVE_VERSION}-1.x86_64.rpm $ODRIVE_ROOT
+
+#if [ -f $ODRIVE_ROOT/odrive.x86_64.rpm ]
+#then
+#  rm $ODRIVE_ROOT/odrive.x86_64.rpm
+#fi
+#ln -s $ODRIVE_ROOT/odrive-${ODRIVE_VERSION}-1.x86_64.rpm $ODRIVE_ROOT/odrive.x86_64.rpm
+
+cd $ODRIVE_ROOT
+
+#actually install it
+rpm -i odrive-${ODRIVE_VERSION}-1.x86_64.rpm
+
