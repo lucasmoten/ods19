f06ada166d78b3f532d40d1c49a22b68857ecd69
Lucas Moten <lucas.moten@deciphertechstudios.com>
2017-01-11 11:48:24 -0500

    Added script to publish rpm built locally to builds.363-283.io bucket Refs #427

--- cmd/odutil/main.go
+++ cmd/odutil/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+
+	"decipher.com/object-drive-server/config"
 )
 
 var (
-	bucket = flag.String("bucket", "odrive-builds", "The bucket to target.")
-	input  = flag.String("input", "", "input file")
-	cmd    = flag.String("cmd", "upload", "Command to run.")
-	key    = flag.String("key", "", "Key up on S3 to write. defaults to the value passed to input")
+	bucket          = flag.String("bucket", "odrive-builds", "The bucket to target.")
+	input           = flag.String("input", "", "input file")
+	cmd             = flag.String("cmd", "upload", "Command to run.")
+	key             = flag.String("key", "", "Key up on S3 to write. defaults to the value passed to input")
+	accessKeyID     = flag.String("accesskeyid", "", "Access Key ID for using S3")
+	secretAccessKey = flag.String("secretaccesskey", "", "Secret Access Key for using S3")
 )
 
 const (
@@ -23,6 +28,19 @@ func main() {
 	//s3gof3r.SetLogger(os.Stdout, "ODUTIL ", 0, true)
 
 	flag.Parse()
+
+	if len(*accessKeyID) > 0 {
+		os.Setenv("AWS_ACCESS_KEY_ID", *accessKeyID)
+	}
+	if len(*secretAccessKey) > 0 {
+		val, err := config.MaybeDecrypt(*secretAccessKey)
+		if err != nil {
+			log.Printf("We cannot decrypt secret access key encoded with the ENC{...} scheme.  Validate that it was encoded with the current token.jar: %v", err)
+			os.Exit(1)
+		}
+		os.Setenv("AWS_SECRET_ACCESS_KEY", val)
+	}
+
 	switch *cmd {
 	case upload:
 		uploadRoutine(*input, *bucket, *key)
