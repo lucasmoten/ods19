fe67f01bd8a98c507a748809adb221884b932982
Rob Fielding <rob.fielding@deciphertechstudios.com>
2017-01-17 12:23:43 -0500

    audit trash search
    gem auditing on trash listing
    
    part of 398

--- server/listObjectsTrashed.go
+++ server/listObjectsTrashed.go
@@ -6,6 +6,7 @@ import (
 
 	"decipher.com/object-drive-server/mapping"
 	"decipher.com/object-drive-server/protocol"
+	"decipher.com/object-drive-server/services/audit"
 
 	"golang.org/x/net/context"
 )
@@ -17,27 +18,42 @@ func (h AppServer) listObjectsTrashed(ctx context.Context, w http.ResponseWriter
 	user, _ := UserFromContext(ctx)
 	dao := DAOFromContext(ctx)
 
+	gem, _ := GEMFromContext(ctx)
+	gem.Action = "access"
+	gem.Payload.Audit = audit.WithType(gem.Payload.Audit, "EventSearchQry")
+	gem.Payload.Audit = audit.WithAction(gem.Payload.Audit, "PARAMETER_SEARCH")
+	gem.Payload.Audit = audit.WithQueryString(gem.Payload.Audit, r.URL.String())
+
 	// Parse paging info
 	pagingRequest, err := protocol.NewPagingRequest(r, nil, false)
 	if err != nil {
-		return NewAppError(400, err, "Error parsing request")
+		herr := NewAppError(400, err, "Error parsing request")
+		h.publishError(gem, herr)
+		return herr
 	}
 
 	// Snippets
 	snippetFields, ok := SnippetsFromContext(ctx)
 	if !ok {
-		return NewAppError(502, errors.New("Error retrieving user permissions"), "Error communicating with upstream")
+		herr := NewAppError(502, errors.New("Error retrieving user permissions"), "Error communicating with upstream")
+		h.publishError(gem, herr)
+		return herr
 	}
 	user.Snippets = snippetFields
 
 	// Get trash for this user
 	results, err := dao.GetTrashedObjectsByUser(user, mapping.MapPagingRequestToDAOPagingRequest(pagingRequest))
 	if err != nil {
-		return NewAppError(500, errors.New("Database call failed: "), err.Error())
+		herr := NewAppError(500, errors.New("Database call failed: "), err.Error())
+		h.publishError(gem, herr)
+		return herr
 	}
 
 	// Response in requested format
 	apiResponse := mapping.MapODObjectResultsetToObjectResultset(&results)
+
+	gem.Payload.Audit = WithResourcesFromResultset(gem.Payload.Audit, results)
+	h.publishSuccess(gem, r)
 
 	// Caller permissions
 	for objectIndex, object := range apiResponse.Objects {
