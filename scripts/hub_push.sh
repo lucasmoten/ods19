2b8814796aebfac1f03a21dd85a0869e7c1bb078
Lucas Moten <lucas.moten@deciphernow.com>
2017-04-24 16:02:45 +0100

    CI metadatadb back to latest. Use jplock/zookeeper. Comment out zk in hub_push

--- scripts/hub_push.sh
+++ scripts/hub_push.sh
@@ -4,7 +4,7 @@
 docker push deciphernow/odrive  
 docker push deciphernow/metadatadb 
 docker push deciphernow/gatekeeper:latest 
-docker push deciphernow/zk:latest 
+# docker push deciphernow/zk:latest 
 docker push deciphernow/aac:latest 
 docker push deciphernow/dias:latest
 
